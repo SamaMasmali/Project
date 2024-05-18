de99aa211e101fe03f5b83c5d5b9c5e2b5f5f1e6
Sama masmali <samamasmali@gmail.com>
2024-05-19 01:00:00 +0300

    Sama1

--- pp.sh
+++ pp.sh
@@ -1,29 +1,61 @@
+#!/bin/bash
 display_menu(){
-echo "Planets App Menu "
-echo "1. Display all planets"
-echo "2. Add a planets"
-echo "3. Remove a planets"
-echo "4. Search for a planets"
+echo "Plants App Menu "
+echo "1. Display all plants"
+echo "2. Add a plants"
+echo "3. Remove a plants"
+echo "4. Search for a plants"
 echo "5. Exit"
 echo -n "Enter your choice"
 }
-display_planets(){
-echo "Lis of planets: "
-cat planets.txt
+display_plants(){
+echo "List of planets: "
+cat planet.txt
 }
-add_planets(){
+add_plants(){
 echo "Enter the name of the new planete: "
-read new_planet
-echo "$new_planet" >> planets.txt
-echo "Planet aded successfully."
+read new_plant
+echo "$new_plant" >> planet.txt
+echo "Plant added successfully."
 }
-search_planet(){
-echo "Enter the name of the planet to search: "
+search_plant(){
+echo "Enter the name of the plant to search: "
 read search_name
-if grep -q "^$search_name" planets.txt; then
+if grep -q "^$search_name" planet.txt; then
 echo "Information about $search_name"
-grep "^$search_name" planets.txt | awk '{print $0}'
+grep "^$search_name" planet.txt | awk '{print $0}'
 else
 echo "Sorry, $search_name not found"
 fi
 }
+remove_plant(){
+echo "Enter the name of the plant to remove"
+read remove_plant
+if grep -q "$remove_plant" planet.txt; then
+sed -i "/$remove_plant/d" planent.txt
+echo "Plant '$remove_plant' removed successfuly."
+else
+echo "Plant '$remove_plant' not found."
+fi
+}
+echo " Welcome to Garden Glow App"
+while true; do 
+display_menu
+echo " Enter your choice: "
+read choice 
+case $choice in 
+1) display_plants
+;;
+2) add_plants
+;;
+3) remove_plant
+;;
+4) search_plant
+;;
+5) echo " Thank you for using the app"
+exit 0
+;;
+*) echo " Invalid Choice "
+;;
+esac
+done
